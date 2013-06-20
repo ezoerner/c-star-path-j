9c4f37b036355233d6146a94d48be22a5e99453c
Eric Zoerner <eric.zoerner@gmail.com>
2013-06-20 11:19:26 +0200

    work in progress on CqlOperations

---
+++ cql/src/main/java/com/ebuddy/cassandra/cql/CqlTemplate.java
@@ -0,0 +1,9 @@
+package com.ebuddy.cassandra.cql;
+
+/**
+ * // TODO: Add class description here.
+ *
+ * @author Eric Zoerner <a href="mailto:ezoerner@ebuddy.com">ezoerner@ebuddy.com</a>
+ */
+public class CqlTemplate implements CqlOperations {
+}
