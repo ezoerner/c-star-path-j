8ae2f42b3da5a0beb725a4f557baeb0ecb6c9b81
Eric Zoerner <eric.zoerner@gmail.com>
2013-06-27 00:13:43 +0200

    remove unused classes

--- cql/src/main/java/com/ebuddy/cassandra/cql/SingleColumnRowMapper.java
+++
@@ -1,9 +0,0 @@
-package com.ebuddy.cassandra.cql;
-
-/**
- * // TODO: Class description.
- *
- * @author Eric Zoerner <a href="mailto:ezoerner@ebuddy.com">ezoerner@ebuddy.com</a>
- */
-public class SingleColumnRowMapper {
-}
