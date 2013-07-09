a7e0291e61e8d335ec2a0dca0378c9ff3b322b3b
Eric Zoerner <eric.zoerner@gmail.com>
2013-07-09 21:47:51 +0200

    added a core module for shared code

---
+++ core/src/main/java/com/ebuddy/cassandra/StructureSupport.java
@@ -0,0 +1,16 @@
+package com.ebuddy.cassandra;
+
+import java.util.Map;
+
+/**
+ * // TODO: Add class description here.
+ *
+ * @author Eric Zoerner <a href="mailto:ezoerner@ebuddy.com">ezoerner@ebuddy.com</a>
+ */
+public class StructureSupport {
+
+    public Map<String,Object> flatten(Map<String,Object> map) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
