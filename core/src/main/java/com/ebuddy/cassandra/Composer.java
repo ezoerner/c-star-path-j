a371b2c0361fbe25689ddc6540134cb452c885b4
Eric Zoerner <eric.zoerner@gmail.com>
2013-07-14 11:36:28 +0200

    composer WIP

--- core/src/main/java/com/ebuddy/cassandra/Composer.java
+++ core/src/main/java/com/ebuddy/cassandra/Composer.java
@@ -1,7 +1,10 @@
 package com.ebuddy.cassandra;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.SortedMap;
 
 
 /**
@@ -15,12 +18,13 @@ public class Composer {
     /**
      * Compose a map of simple objects keyed by paths into a single complex object.
      *
-     * @param decomposedObjects input map of decomposed objects, paths mapped to simple values
+     * @param decomposedObjects input map of decomposed objects, paths mapped to simple values.
+     *                          Sorted by the path keys to facilitate list reconstruction.
      * @return a complex object such as a map or list decoded from the paths in decomposedObjects,
      *         or null if decomposedObjects is empty.
      * @throws IllegalArgumentException if there are unsupported objects types in decomposedObjects.
      */
-    public Object compose(Map<Path,Object> decomposedObjects) {
+    public Object compose(SortedMap<Path,Object> decomposedObjects) {
         // assume that the first key determines the type and that the data will be self-consistent.
         // If we encounter an inconsistency then we will backtrack.
         Iterator<Map.Entry<Path,Object>> iterator = decomposedObjects.entrySet().iterator();
@@ -30,7 +34,10 @@ public class Composer {
         Map.Entry<Path,Object> firstEntry = iterator.next();
         Path firstKey = firstEntry.getKey();
         if (firstKey.isListIndex()) {
-            return composeList(decomposedObjects);
+            List<Object> list = composeList(decomposedObjects);
+            if (list != null) {
+                return list;
+            }
         }
         return composeMap(decomposedObjects);
     }
@@ -38,8 +45,23 @@ public class Composer {
     //////// Private Methods //////////
 
 
-    private Object composeList(Map<Path,Object> decomposedObjects) {
-        throw new UnsupportedOperationException("Not yet implemented");
+    /**
+     * @return null if cannot make a list from the data
+     */
+    private List<Object> composeList(SortedMap<Path,Object> decomposedObjects) {
+        List<Object> list = new ArrayList<Object>(decomposedObjects.size());
+        for (Map.Entry<Path,Object> entry : decomposedObjects.entrySet()) {
+            // regardless of the actual indexes found in the paths, assign to list based on
+            // sorted paths since some indexes may have been deleted. Sparse lists not supported.
+            Path path = entry.getKey();
+            if (!path.isListIndex()) {
+                // could not get a valid index for this path (so we will make a map at this level instead)
+                return null;
+            }
+            list.add(entry.getValue());
+        }
+        return list;
+        // TODO: we need to advance and propagate the paths somehow...
     }
 
     private Object composeMap(Map<Path,Object> decomposedObjects) {
--- core/src/main/java/com/ebuddy/cassandra/Path.java
+++ core/src/main/java/com/ebuddy/cassandra/Path.java
@@ -1,14 +1,13 @@
 package com.ebuddy.cassandra;
 
 import java.util.Arrays;
-import java.util.Iterator;
 
 /**
  * // TODO: Add class description here.
  *
  * @author Eric Zoerner <a href="mailto:ezoerner@ebuddy.com">ezoerner@ebuddy.com</a>
  */
-public class Path {
+public class Path implements Comparable<Path> {
     private static final char PATH_DELIMITER_CHAR = '|';
     private static final String LIST_INDEX_PREFIX = "@";
 
@@ -30,22 +29,34 @@ public class Path {
     }
 
     /**
-     * Return true if the first element in this path is a list index.
+     * Return true if the head element in this path is a list index.
      */
     public boolean isListIndex() {
-        return pathElements[0].startsWith(LIST_INDEX_PREFIX);
+        String head = pathElements[0];
+        if (!head.startsWith(LIST_INDEX_PREFIX)) {
+            return false;
+        }
+        String rest = head.substring(LIST_INDEX_PREFIX.length());
+        int index;
+        try {
+            index = Integer.parseInt(rest);
+        } catch (NumberFormatException ignored) {
+            return false;
+        }
+        return index >= 0;
     }
 
+    /**
+     * Paths always end with the delimiter character in order to facilitate
+     * start/finish slice queries in Cassandra.
+     * @return the String representation of a path.
+     */
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
-        Iterator<String> itr = Arrays.asList(pathElements).iterator();
-        while (itr.hasNext()) {
-            String pathElement = itr.next();
+        for (String pathElement : Arrays.asList(pathElements)) {
             builder.append(pathElement);
-            if (itr.hasNext()) {
-                builder.append(PATH_DELIMITER_CHAR);
-            }
+            builder.append(PATH_DELIMITER_CHAR);
         }
         return builder.toString();
     }
@@ -59,5 +70,10 @@ public class Path {
             return (Path)o;
         }
         return new Path(o.toString());
+    }
+
+    @Override
+    public int compareTo(Path o) {
+        return toString().compareTo(o.toString());
     }
 }
