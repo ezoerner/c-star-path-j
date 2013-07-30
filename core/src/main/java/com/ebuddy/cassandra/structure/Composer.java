c69c5537e16c6e36a676826bd87ab6c3d1047157
Eric Zoerner <eric.zoerner@gmail.com>
2013-07-30 13:35:41 +0200

    updated javadocs EBD-5049

--- core/src/main/java/com/ebuddy/cassandra/structure/Composer.java
+++ core/src/main/java/com/ebuddy/cassandra/structure/Composer.java
@@ -12,22 +12,22 @@ import java.util.TreeMap;
 
 
 /**
- * Support for composing paths to objects back  complex objects.
+ * Support for composing paths back to complex objects.
  * Only the basic JSON structures are supported, i.e. Maps, Lists, Strings, Numbers, Booleans, and null.
  *
- * It is possible to write data that will cause inconsistencies in on object structure
- * when it is reconstructed when read. This implementation will resolve inconsistencies as follows:
+ * It is possible to write data that will cause inconsistencies in an object structure
+ * when it is reconstructed. This implementation will resolve inconsistencies as follows:
  *
  * If data objects are found at a particular path as well as longer paths, the data object
  * is returned in a map structure with the special key "@ROOT". This may cause an error
- * if the data is attempted to be deserialized into a POJO.
+ * if the data is later attempted to be deserialized into a POJO.
  *
  * If list elements are found at the same level as longer paths or a data object, then
  * the list elements are returned in a map with the index as keys in the map, e.g. "@0", "@1",
  * etc.
  *
  * If inconsistencies such as these are preventing data from being deserialized into a
- * particular POJO, the data can always be retrieved using an instance of TypeReference<Object>,
+ * particular POJO, the data can always be retrieved using an instance of (a sublcass of) TypeReference<Object>,
  * which will return the basic JSON to Java mappings, i.e. Maps, Lists and Strings, etc.
  *
  * @author Eric Zoerner <a href="mailto:ezoerner@ebuddy.com">ezoerner@ebuddy.com</a>
