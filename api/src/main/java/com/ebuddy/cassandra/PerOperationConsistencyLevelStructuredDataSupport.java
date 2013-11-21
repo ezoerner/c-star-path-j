5b8c83c170f6396905e3cbc4c77116a488c4f18b
Eric Zoerner <eric.zoerner@gmail.com>
2013-11-21 14:30:18 +0100

    skeleton of interface for per operation consistency level
    inssue #27

---
+++ api/src/main/java/com/ebuddy/cassandra/PerOperationConsistencyLevelStructuredDataSupport.java
@@ -0,0 +1,10 @@
+package com.ebuddy.cassandra;
+
+/**
+ * Interface for supporting the specification of the consistency level on every operation
+ * of StructuredDataSupport.
+ *
+ * @author Eric Zoerner <a href="mailto:ezoerner@ebuddy.com">ezoerner@ebuddy.com</a>
+ */
+public class PerOperationConsistencyLevelStructuredDataSupport {
+}
